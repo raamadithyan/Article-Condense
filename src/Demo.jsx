c1dbe1df8b95f9f40e94d0d855d046c51ab06d74
raamadithyan <chitha.don@gmail.com>
2023-09-12 21:41:53 +0530

    input added

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,7 +1,41 @@
+import {useState,useEffect} from 'react';
+import { copy, linkIcon,loader,tick } from './assets';
 export default function Demo() {
+	const[article,setArticle]=useState({
+		url:"",
+		summary:""
+	})
+
+	async function handleSubmit(){
+		alert("submitted")
+	}
 return (
- <div>
-Demo
-</div>
+ <section className='mt-16 w-full max-w-xl'>
+ <div className=' flex flex-col w-full gap-2'>
+ 	
+ 	<form 
+ 	className=
+ 	"relative flex justify-center items-center " 
+ 	onSubmit={handleSubmit}
+ 	action="">
+ 	<img src={linkIcon} alt="link_icon"
+className='absolute left-0 my-2 ml-3 w-5'
+ 	/>
+
+ 	<input type="url"
+ 	  placeholder='Enter a url'
+ 	  value={article.url}
+ 	  onChange={(e)=> setArticle({...article,url:e.target.value})}
+ 	  required
+ 	  className='url_input peer'
+ 	 />
+ 	<button type='submit'
+ 	 className="submit_btn peer-focus:border-gray-400 peer-focus:text-green-400">
+ 		↵
+ 	</button>
+
+ 	</form>
+ </div>
+</section>
 );
 }
